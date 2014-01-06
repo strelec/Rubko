1bd7e571d707f819130b7cc4f26b1cecf998cfba
strelec <rok.kralj@gmail.com>
2014-01-06 13:25:43 +0100

    Added Mutex to MemoryStoragePlugin.

--- lib/rubko/plugins/storage/memory.rb
+++ lib/rubko/plugins/storage/memory.rb
@@ -3,6 +3,55 @@ require 'rubko/storage'
 class MemoryStoragePlugin < Rubko::Storage
 
 	@@data = {}
+	@@mutex = Mutex.new
+
+	def [](*path)
+		@@mutex.synchronize {
+			find(path)[nil]
+		}
+	end
+
+	def []=(*path, val)
+		path.map! { |x| escape x }
+		@@mutex.synchronize {
+			path.reduce( @@data ) { |v, k|
+				v[k] ||= {}
+				if k.equal? path.last # is the last element
+					v[k][nil] = val
+				end
+				v[k]
+			}
+		}
+		val
+	end
+
+	def keys(*path)
+		@@mutex.synchronize {
+			find(path).keys.compact.map { |x|
+				unescape x
+			}
+		}
+	end
+
+	def prune(*path)
+		last = escape path.pop
+		@@mutex.synchronize {
+			find(path).delete last
+		}
+	end
+
+	def rename(*path, name)
+		name = escape name
+		last = escape path.pop
+		@@mutex.synchronize {
+			hash = find path
+			return false if hash[name]
+			hash[name] = hash.delete last
+		}
+		true
+	end
+	
+private
 
 	def find(path)
 		path.map { |x|
@@ -18,43 +67,5 @@ class MemoryStoragePlugin < Rubko::Storage
 
 	def unescape(key)
 		key.to_sym
-	end
-
-	private :find, :escape, :unescape
-
-	def [](*path)
-		find(path)[nil]
-	end
-
-	def []=(*path, val)
-		path.map! { |x| escape x }
-		path.reduce( @@data ) { |v, k|
-			v[k] ||= {}
-			if k.equal? path.last # is the last element
-				v[k][nil] = val
-			end
-			v[k]
-		}
-		val
-	end
-
-	def keys(*path)
-		find(path).keys.compact.map { |x|
-			unescape x
-		}
-	end
-
-	def prune(*path)
-		last = escape path.pop
-		find(path).delete last
-	end
-
-	def rename(*path, name)
-		name = escape name
-		last = escape path.pop
-		hash = find path
-		return false if hash[name]
-		hash[name] = hash.delete last
-		true
 	end
 end
